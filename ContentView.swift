ac48863139ee1ddd106a3ce3ce2b7abcd7052d79
mohammed eloksh <mmaeloksh@gmail.com>
2019-11-29 14:34:11 +0200

    MAR Progect

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MAR
+//
+//  Created by macbookpro on 11/29/19.
+//  Copyright © 2019 macbookpro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
